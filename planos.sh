cba285a9912594f2a7e3d6f9d7ae25f3197d6f9c
i-rochiño <irocho.profe@gmail.com>
2017-03-21 16:10:00 +0100

    Pasar a primeiro e segundo plano

---
+++ planos.sh
@@ -0,0 +1,17 @@
+#!
+########################################################
+#
+# Pasar a primeiro e segundo plano
+#
+# autor:
+########################################################
+
+
+# arranca xclock en segundo plano
+
+# comproba os procesos que están en segundo plano
+
+# mostra os procesos que están executándose
+
+# pasa a xclock a primeiro plano
+
