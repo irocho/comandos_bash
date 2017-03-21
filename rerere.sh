dcef0f9ef6f1bc1e25a441e4a2a27fd8c77fa751
i-rochiño <irocho.profe@gmail.com>
2017-03-21 16:56:06 +0100

    Create rerere.sh

---
+++ rerere.sh
@@ -0,0 +1,15 @@
+#!
+##############################################################
+#
+# Redirecionamento
+#
+# autor:
+#############################################################
+
+# fabrica un ficheiro que o seu contido sexa a saída de ls
+
+# mostra no terminal o contido dese ficheiro
+
+# fabrica un ficheiro que o seu contido sexa a páxina do manual de pwd
+
+# fabrica un ficheiro que o seu contido sexa a saída do comando uname nas primeiras liñas e a saída de whoami na segunda
