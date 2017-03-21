3b020a5f014fdefa74ab443d7f1dfa78aa4d004d
i-rochiño <irocho.profe@gmail.com>
2017-03-21 16:40:22 +0100

    novo

---
+++ aaab.sh
@@ -0,0 +1,15 @@
+#!
+##############################################################
+#
+# Cambiar nome e copiar ficheiros
+#
+# autor:
+#############################################################
+
+# mostra o contido de /tmp
+
+# crea un ficheiro dentro de /tmp chamado ttt.txt
+
+# cámbialle o nome a xxx.txt
+
+# copia o ficheiro xxx.txt ó directorio no que estás
