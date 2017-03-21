37ec0cabab86bb5ba7d44878581348e608caad2a
i-rochiño <irocho.profe@gmail.com>
2017-03-21 16:47:19 +0100

    Create ppp.sh

---
+++ ppp.sh
@@ -0,0 +1,16 @@
+#!
+##############################################################
+#
+# Pipe ou tuberías
+#
+# autor:
+#############################################################
+
+# mostra a saída do comando ls
+
+# mostra só as  primeiras liñas da saída de ls
+
+# mostra o man do comando pwd
+
+# mostra só as derradeiras liñas do manual de pwd
+
