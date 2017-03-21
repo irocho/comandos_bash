d3a033d06b841ba2cef00956fc3b879fa1a3fd46
i-rochiño <irocho.profe@gmail.com>
2017-03-21 17:13:14 +0100

    Create axuda.sh

---
+++ axuda.sh
@@ -0,0 +1,14 @@
+#!
+##############################################################
+#
+# Móstralle ó usuario o manual do comando que pide 
+#
+# autor:
+#############################################################
+
+
+# pregunta ó usuario cal é o comando que lle interesa
+
+# mostra a páxina do manual dese comando
+
+# fabrica un ficheiro coa información que saiu no terminal
