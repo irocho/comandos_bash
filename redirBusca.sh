20c1ec5454e2492581932881814d8a6308cf5b8a
i-rochiño <irocho.profe@gmail.com>
2017-03-21 16:03:26 +0100

    Update and rename ele_ese.sh to redirBusca.sh

---
+++ redirBusca.sh
@@ -0,0 +1,13 @@
+#!
+########################################################
+#
+# Redirecionamento e busca nun ficheiro
+#
+# autor:
+########################################################
+
+# manda a un ficheiro chamado hhh.txt a saída do comando history
+
+# averigua cantas veces está ls en hhh.txt
+
+
