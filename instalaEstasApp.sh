47c487cc2bb9dc11d1e7138b6399803f84ee6c66
i-rochiño <irocho.profe@gmail.com>
2017-03-21 17:08:14 +0100

    Create instalaEstasApp.sh

---
+++ instalaEstasApp.sh
@@ -0,0 +1,17 @@
+#!
+##############################################################
+#
+# Script que instala un programa
+#
+# autor:
+#############################################################
+
+# actualiza os programas e a base de datos
+
+# instala o programa tree
+
+# comproba que está instalado
+
+# instala o programa terminator
+
+# comproba que está instalado
