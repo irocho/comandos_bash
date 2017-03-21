f736f990cf24ad4438fa6858468bd7bd8cb2c862
i-rochiño <irocho.profe@gmail.com>
2017-03-21 16:31:47 +0100

    parte do exame

---
+++ UsuarioDirFich.sh
@@ -0,0 +1,26 @@
+#!
+#############################################################
+#
+# Interación co usuario para crear directorios e carpetas
+#
+# autor:
+#############################################################
+
+# crea dentro do directorio /tmp un novo directorio co nome que lle quera dar o usuario
+
+# dentro do directorio dous ficheiros cos nomes que desexe o  usuario 
+
+
+# o primeiro ficheiro será a saída do comando history 
+
+# o segundo ficheiro será o listado do directorio /etc
+
+# engade todos os permisos a todos os usuarios a eses dous ficheiros
+ 
+# pregunta ó usuario o nome que lle quere dar a un paquete 
+
+# crea dentro do directorio o paquete que conteña os dous ficheiros co nome anterior
+
+# comprime o paquete
+
+# borra os ficheiros e deixa só o paquete no directorio
