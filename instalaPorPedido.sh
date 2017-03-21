bb4ce63357d9371c52c99847267b49f92c7fef29
i-rochiño <irocho.profe@gmail.com>
2017-03-21 17:10:17 +0100

    Create instalaPorPedido.sh

---
+++ instalaPorPedido.sh
@@ -0,0 +1,14 @@
+#!
+##############################################################
+#
+# Instala os paquetes que solicite o usuario
+#
+# autor:
+#############################################################
+
+# Pregunta ó usuario o nome do paquete que quere instalar
+
+# Actualiza base de datos e paquetes xa instalados
+
+# Comproba que instalou o paquete pedido
+
