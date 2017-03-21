0296bd5588c1bbc57222691b70136f7c0d0f7260
i-rochiño <irocho.profe@gmail.com>
2017-03-21 16:51:24 +0100

    Create buscar.sh

---
+++ buscar.sh
@@ -0,0 +1,17 @@
+#!
+##############################################################
+#
+# Buscar nun ficheiro
+#
+# autor:
+#############################################################
+
+# fabrica con cat un ficheiro que teña alomenos 12 liñas de texto con nomes de animais
+
+# busca nese ficheiro se aparece a palabra gato
+
+# busca nese ficheiro se aparece a palabra can
+
+# busca nese ficheiro cantas veces aparece a letra m
+
+# busca nese ficheiro cantas veces aparece a letra M ou ben a letra m
