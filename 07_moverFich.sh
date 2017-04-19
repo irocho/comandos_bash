a5bca1667f98b15d40e083010a8413aa14442011
i-rochiño <irocho.profe@gmail.com>
2017-04-19 13:43:40 +0200

    Create 07_moverFich.sh

---
+++ 07_moverFich.sh
@@ -0,0 +1,16 @@
+#! 
+##### ####### ####### ####### ###
+#
+# Crear carpetas e mover un ficheiro entre elas
+# autor:
+#
+### #### ### ####### ####### ####### ####
+
+
+# Crea dúas carpeta que se chamen carp1/ e outra  carp2/
+
+
+# Crea o ficheiro xxx.dat na carpeta carp1/
+
+
+# Move o ficheiro xxx.dat á carpeta carp2/
