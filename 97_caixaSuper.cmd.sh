49051d777aeb4d79290efb1a6a32c805ae75f149
i-rochiño <irocho.profe@gmail.com>
2017-04-26 17:50:54 +0200

    Caixa rápida do súper (esquelete)

--- 97_caixaSuper.cmd.sh
+++ 97_caixaSuper.cmd.sh
@@ -20,13 +20,12 @@
 
 
 # Pide  o prezo do primeiro producto
-read p1
+
 
 # Pide  o prezo do segundo producto
-read p2
+
 
 # Pide o prezo do terceiro producto
-read p3
+
 
 # Mostra na pantalla a suma dos tres prezos
-echo $((p1+p2+p3))
