540f32c064f591eb46edc9931b194698e4ea32a8
i-rochiño <irocho.profe@gmail.com>
2017-04-26 17:49:24 +0200

    subido

---
+++ 100_caixaSuper.cmd.sh
@@ -0,0 +1,32 @@
+
+
+
+# Engade arriba a primeira liña de todos os scripts
+
+#
+############################################################
+#
+# Título: Programando a caixa rápida dun supermercado
+#
+# Nome do autor:
+#
+# Enderezo do blog: 
+#
+# Sistema operativo: 
+#
+#############################################################
+
+# Saúda educadamente ó usuario
+
+
+# Pide  o prezo do primeiro producto
+read p1
+
+# Pide  o prezo do segundo producto
+read p2
+
+# Pide o prezo do terceiro producto
+read p3
+
+# Mostra na pantalla a suma dos tres prezos
+echo $((p1+p2+p3))
