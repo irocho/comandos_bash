7f1f9e1061db8256eab6676c2afff16907e2131e
i-rochiño <irocho.profe@gmail.com>
2017-05-08 13:27:35 +0200

    Create 31_menu.sh

---
+++ 31_menu.sh
@@ -0,0 +1,21 @@
+############################################################
+#
+# Título: Menú para que escolla o usuario
+#
+# Nome do autor:
+#
+# Enderezo do blog: 
+#
+# Sistema operativo: 
+#
+#############################################################
+
+# Mostrar ó usuario un menú para crear directorio, un ficheiro ou pedir axuda
+
+# Preguntar ó usuario o que quere facer
+
+# se quere crear directorio, preguntarlle o nome
+
+# se quere crear un ficheiro, preguntarlle o nome
+
+# se quere axuda , preguntarlle o nome do comando
