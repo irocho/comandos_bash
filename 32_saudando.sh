27277fb0dd9f639084119fb31014b57ecf70de63
i-rochiño <irocho.profe@gmail.com>
2017-05-11 00:13:42 +0200

    condicional sinxeliño

---
+++ 32_saudando.sh
@@ -0,0 +1,17 @@
+############################################################
+#
+# Título: Saudar como corresponde
+#
+# Nome do autor:
+#
+# Enderezo do blog: 
+#
+# Sistema operativo: 
+#
+#############################################################
+
+# Pregunta o nome do usuario
+
+# Se a resposta é Pepe saúdao polo seu nome
+
+# Se a resposta é calquera outro nome dalle os bos días
