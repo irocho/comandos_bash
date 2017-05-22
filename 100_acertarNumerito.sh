c6eef5989b7cb353e4fb5cda2b3f6b14c6b61546
i-rochiño <irocho.profe@gmail.com>
2017-05-22 15:19:05 +0200

    Create 100_acertarNumerito.sh

---
+++ 100_acertarNumerito.sh
@@ -0,0 +1,30 @@
+#! 
+#####################################################################################################
+#
+# Xogo que xenera un número e dálle varias oportunidades ó usuario para que o acerte
+#
+# autor:
+#
+###################################################################################################
+
+
+# Xenerar un número aleatorio de 2 díxitos
+
+
+# oportunidades para xogar
+for i in 1 2 3
+do
+	# pregunto o número ó usuario
+
+
+	# en caso de que acerte
+	if 
+		then 
+			
+	fi
+
+	# cando sexa maior o misterioso
+
+	# cando sexa menor o misterioso
+
+done
